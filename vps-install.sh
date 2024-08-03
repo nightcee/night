693da57347762813b434595e44a419450b36070c
nightcee <39356959+nightcee@users.noreply.github.com>
2024-08-04 01:17:48 +0800

    Add files via upload

---
+++ vps-install.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+echo "Installing unzip"
+sudo apt install unzip -y
+echo "unzip installed"
+echo "Fetching latest Hemis version"
+wget --quiet https://github.com/Hemis-Blockchain/Hemis/releases/latest/download/Hemis-Linux.zip && sudo unzip Hemis-Linux.zip -d /usr/local/bin
+wget --quiet https://github.com/Hemis-Blockchain/Hemis/releases/download/1.1.6/Hemis-params.zip && unzip Hemis-params.zip -d ~/.Hemis-params
+echo "Cleanup excess files"
+rm Hemis-Linux.zip && rm Hemis-params.zip
+Hemisd || true
+sleep 4
+Hemis-cli stop
+echo "Hemis successfully configured."
+exit
